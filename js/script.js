8d48cd6d7518198f2ad458b8d76458dfc792b423
Jonny Thomas-Davies <jonnytdavies@aol.com>
2020-04-09 14:35:57 +0000

    Updated CSS to display listing results after search, and changed hotel icon

--- js/script.js
+++ js/script.js
@@ -61,8 +61,7 @@ function initMap() {
                 // Create a marker for each hotel found, and
                 // assign a letter of the alphabetic to each marker icon.
                 for (var i = 0; i < results.length; i++) {
-                    var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-                    var markerIcon = MARKER_PATH + markerLetter + '.png';
+                    var markerIcon = "http://maps.google.com/mapfiles/kml/pal2/icon20.png";
                     // Use marker animation to drop the icons incrementally on the map.
                     markers[i] = new google.maps.Marker({
                         position: results[i].geometry.location,
@@ -99,8 +98,7 @@ function initMap() {
 
     function addResult(result, i) {
         var results = document.getElementById('results');
-        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        var markerIcon = "http://maps.google.com/mapfiles/kml/pal2/icon20.png";
 
         var tr = document.createElement('tr');
         tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
